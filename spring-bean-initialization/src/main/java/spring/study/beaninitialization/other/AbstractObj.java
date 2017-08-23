4f28694886314334fa737f09a1bb6135a3116328
Format <fangjian0423@gmail.com>
2017-08-23 15:43:17 +0800

    remove unuseful module

--- spring-bean-initialization/src/main/java/spring/study/beaninitialization/other/AbstractObj.java
+++
@@ -1,9 +0,0 @@
-package spring.study.beaninitialization.other;
-
-import org.springframework.stereotype.Component;
-
-/**
- * Created by Format on 2017/6/17.
- */
-public abstract class AbstractObj {
-}
